209dbd72d55827d550d9d2d6b30db69c3358565e
Alan Sá <alkinsret@gmail.com>
2025-06-18 18:35:32 -0300

    refactor: get post by slug

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,20 +1,12 @@
 import { findPostBySlugCached } from '@/lib/post/queries';
-import { notFound } from 'next/navigation';
 
 type PostSlugPageProps = {
   params: Promise<{ slug: string }>;
 };
 export default async function PostSlugPage({ params }: PostSlugPageProps) {
   const { slug } = await params;
-  let post;
+  const post = await findPostBySlugCached(slug);
 
-  try {
-    post = await findPostBySlugCached(slug);
-  } catch {
-    post = undefined;
-  }
-
-  if (!post) notFound();
   return (
     <div>
       <p>{post.title}</p>
